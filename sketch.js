c9def03b5d677e218fb9d01a1fd18bb9cc4af258
Sarkar2008 <93257392+Sarkar2008@users.noreply.github.com>
2021-12-01 15:12:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var std1, std2;
+function setup() {
+  createCanvas(400,400);
+  std1 = new Student ("Evelina", 21, 14, 7);
+  std1.display();
+  std2 = new Student ("Meryl", 34, 14, 7);
+  std2.display();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,18 @@
+
+class Student
+{ constructor( name, rollno, age, grade){
+    this.name = name;
+    this.rollno = rollno;
+    this.age = age;
+    this.grade = grade;
+}
+   display(){
+       console.log(this.name);
+       console.log(this.rollno);
+       console.log(this.age);
+       console.log(this.grade);
+   }
+}
+
+
+
